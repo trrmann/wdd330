cc0049e860e9f67a8fcdbab324630dd511aff31c
trrmann <113477307+trrmann@users.noreply.github.com>
2025-10-26 12:18:54 -0400

    update core required tags in base page

---
+++ scripts/getDates.js
@@ -0,0 +1,18 @@
+// Get the current date
+const currentYear = new Date().getFullYear();
+// Get the documents last modified date
+const lastModDate = document.lastModified;
+
+// Get the currentYear element from html
+const currentYearElement = document.querySelector('.currentyear');
+// Get the lastModified element from html
+const lastModElement = document.querySelector('.lastModified');
+
+// update the html element with the data
+if (currentYearElement) {
+    currentYearElement.innerHTML = `&copy; ${currentYear}`;
+}
+// update the html element with the data
+if (lastModElement) {
+    lastModElement.innerHTML = `Last modification:  ${lastModDate}`;
+}
