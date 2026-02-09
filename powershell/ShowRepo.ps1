cee868291921c99d25cc0691d1abdc3821227889
trrmann <trmann70@hotmail.com>
2026-02-08 19:28:12 -0500

    fixed the show repo script

--- powershell/ShowRepo.ps1
+++ powershell/ShowRepo.ps1
@@ -1,9 +1,54 @@
-# ShowRepo.ps1
+## ShowRepo.ps1
 # Opens the GitHub repository page in the default browser.
 # Usage: .\ShowRepo.ps1
 
-# Get the remote URL
-gitRemote = git config --get remote.origin.url
+$gitRemote = $(git config --get remote.origin.url)
+
+if (-not $gitRemote) {
+    Write-Host "No remote 'origin' found."
+    exit 1
+}
+
+# Convert SSH or HTTPS URL to web URL
+if ($gitRemote -match "^git@github.com:(.+)\.git$") {
+    $repoPath = $Matches[1]
+    $webUrl = "https://github.com/$repoPath"
+} elseif ($gitRemote -match "^https://github.com/(.+)\.git$") {
+    $repoPath = $Matches[1]
+    $webUrl = "https://github.com/$repoPath"
+} else {
+    Write-Host "Unrecognized remote URL format: $gitRemote"
+    exit 1
+}
+
+Write-Host "Opening $webUrl in your default browser..."
+Start-Process $webUrl
+
+Write-Host "Opening $webUrl in your default browser..."
+Start-Process $webUrl
+$gitRemote = $(git config --get remote.origin.url)
+
+if (-not $gitRemote) {
+    Write-Host "No remote 'origin' found."
+    exit 1
+}
+
+# Convert SSH or HTTPS URL to web URL
+if ($gitRemote -match "^git@github.com:(.+)\.git$") {
+    $repoPath = $Matches[1]
+    $webUrl = "https://github.com/$repoPath"
+} elseif ($gitRemote -match "^https://github.com/(.+)\.git$") {
+    $repoPath = $Matches[1]
+    $webUrl = "https://github.com/$repoPath"
+} else {
+    Write-Host "Unrecognized remote URL format: $gitRemote"
+    exit 1
+}
+
+Write-Host "Opening $webUrl in your default browser..."
+Start-Process $webUrl
+## Get the remote URL
+$gitRemote = $(git config --get remote.origin.url)
 
 if (-not $gitRemote) {
     Write-Host "No remote 'origin' found."
